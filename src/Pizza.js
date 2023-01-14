ba9925a08395502c9981384f054c51882a2538fc
Munsa1 <munsamibenge65@gmail.com>
2023-01-15 00:01:35 +0200

    Add component structure, app logic and state management

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react'
-import pizza from './pizza.jpeg'
+import pizza from './pizza.png'
+
+const Counter = {
+    fontSize: "82px",
+    backgroundColor: "black",
+    borderRadius: "20px",
+}
+
+const Button = {
+    fontSize: "28px",
+    padding: "10px",
+    backgroundColor: "white",
+    borderRadius: "10px"
+}
 export class Pizza extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0 
+        }
+    }
+    
+    takeOrder(){
+        this.setState({
+            count:this.state.count + 1
+        })
+    }
+
+    cancelOrder(){
+        this.setState({
+            count:this.state.count - 1
+        })
+    }
+
   render() {
-    return (
+    return (    
       <div>
-          <img src={pizza} className="app-logo" alt="Pizza" />
-      </div>
-      
+          <img src={pizza} className="app-logo" alt="Pizza" width="200"/>
+          <h1>Pizza Order Counter</h1>
+          <h1 style={Counter}>{this.state.count}</h1>
+          <button style={Button} onClick={()=>this.takeOrder()}>Take Order</button>
+          <br />
+          <br />
+          <button style={Button} onClick={()=>this.cancelOrder()}>Cancel Order</button>
+      </div> 
     )
   }
 }
